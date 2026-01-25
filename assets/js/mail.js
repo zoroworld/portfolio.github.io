b7c066e43e22f877e9c4a29fe8a861145a7e6539
Kali <manishpc@localhost>
2026-01-25 21:47:39 +0530

    update portfolio

--- assets/js/mail.js
+++ assets/js/mail.js
@@ -1,24 +1,32 @@
 (function () {
   "use strict";
 
-  document.getElementById("contact-form").addEventListener("submit", async function(e) {
-    e.preventDefault();
-    const form = e.target;
-    const data = new FormData(form);
-  
-    const response = await fetch(form.action, {
-      method: form.method,
-      body: data,
-      headers: { "Accept": "application/json" }
+  const loading = document.querySelector(".loading");
+  const errorMessage = document.querySelector(".error-message");
+  const successMessage = document.querySelector(".sent-message");
+
+  document
+    .getElementById("contact-form")
+    .addEventListener("submit", async function (e) {
+      e.preventDefault();
+      loading.style.display = "block";
+      const form = e.target;
+      const data = new FormData(form);
+
+      const response = await fetch(form.action, {
+        method: form.method,
+        body: data,
+        headers: { Accept: "application/json" },
+      });
+
+      if (response.ok) {
+        loading.style.display = "none";
+        successMessage.style.display = "block";
+        //   alert("Form submitted successfully!");
+        form.reset();
+      } else {
+        errorMessage.style.display = "block";
+        //   alert("Failed to submit form. Try again.");
+      }
     });
-  
-    if (response.ok) {
-      alert("Form submitted successfully!");
-      form.reset();
-    } else {
-      alert("Failed to submit form. Try again.");
-    }
-  });
-
-
 })();
